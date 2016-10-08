9ed394852324faad398a6b770deaf08abaacbd17
Dillon Hafer <dh@dillonhafer.com>
2016-10-08 14:52:28 -0500

    Add SimpleCov artifacts to CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'simplecov'
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
