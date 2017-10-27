28e7c8528392b0e2884aef938620b9d8c3291a0c
David Simon <davidsimon@swellinvesting.com>
2017-10-27 13:39:22 -0700

    Adding permissive CORS headers to mocked response

--- spec/capybara/proxy_spec.rb
+++ spec/capybara/proxy_spec.rb
@@ -14,13 +14,20 @@ describe Capybara::Webmock::Proxy do
 
     let(:proxy) { Capybara::Webmock::Proxy.new('123456') }
 
+    let(:expected_headers) { {
+      'Content-Type' => 'text/html',
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Methods' => '*',
+      'Access-Control-Allow-Headers' => '*'
+    } }
+
     before do
       allow_any_instance_of(Rack::Proxy).to receive(:perform_request).and_return(['400', {foo: :bar}, ['baz']])
     end
 
     it 'returns an empty response when unknown host' do
       env = new_env("notlvh.me")
-      expect(proxy.perform_request(env)).to eq ["200", {"Content-Type"=>"text/html"}, [""]]
+      expect(proxy.perform_request(env)).to eq ["200", expected_headers, [""]]
     end
 
     %w{lvh.me sub.lvh.me localhost 127.0.0.1}.each do |host|
